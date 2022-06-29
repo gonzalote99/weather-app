25f7897a114f2574b34f8f33e0b974f2cd218f45
gonzalote99 <gonzalodavila759@gmail.com>
2022-06-29 17:13:01 -0300

    1 file modified

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+let input=document.querySelector('.city-input');
+let weatherData = document.querySelector('.weather-data');
+let btn=document.querySelector('.btn-search');
+btn.addEventListener('click', () => {
+  displayWeather();
+})
 
+document.addEventListener('keypress', (event) => {
+ if(event.keyCode === 13) {
+   displayWeather();
+ }
+})
+
+const renderLoader = () => {
+  let loader = `
+  <div class="d-flex justify-content-center m-2">
+   <div class="spinner-border spinner " role="status"> 
+    <span class="sr-only">loading..</span> 
+   </div>
+  </div>
+  `;
+  weatherData.insertAdjacentHTML('beforebegin', loader)
+}
+
+const clearLoder = () => {
+  
+}
